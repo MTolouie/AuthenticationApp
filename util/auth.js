fa89513f7ef348e2f18c90cbbbfbccac31b0ca40
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2024-02-12 13:21:17 +0300

    added login logic to the auth.js file and loginScreen

--- util/auth.js
+++ util/auth.js
@@ -1,22 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = "AIzaSyCccvYNSO5EiwtGGgq7tfoD2khPoYLZUXY";
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
+  try {
+    const response = await axios.post(url, {
       email: email,
       password: password,
-      returnSecureToken: true
+      returnSecureToken: true,
+    });
+    console.log(response.data);
+  } catch (error) {
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.error &&
+      error.response.data.error.message
+    ) {
+      console.error(
+        "Error While Authenticating:",
+        error.response.data.error.message
+      );
+    } else {
+      console.error("An error occurred while authenticating:", error.message);
     }
-  );
+    throw error;
+  }
+};
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
 }
 
-
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
+}
 // export async function createUser(email, password) {
 //   console.log(email + "and" + password);
-  
+
 //   try {
 //     const response = await axios.post(
 //       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
