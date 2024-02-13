c223bd90dc14d8b56bfd2e35893247368a42574d
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2024-02-13 12:08:07 +0300

    added get request with token in welcomeScreen file

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState();
+  const token = useSelector((state) => state.user.token);
+  useEffect(() => {
+    axios
+      .get(
+        `https://expensetrackerapp-a44ea-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +29,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
